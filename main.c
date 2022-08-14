525aff9b491f128cbc3783e2ca32419c51756c84
mostafanasrat4 <mostafanasrat4@gmail.com>
2022-08-15 01:24:58 +0200

    added test cases + fix wrong output

--- main.c
+++ main.c
@@ -5,12 +5,12 @@
 #define SPACER printf("----------------------------------------------\n")
 
 int checkPair(char item1,char item2);
-int isParenthesisBalanced(char exp[]);
+int isBracketsBalanced(char exp[]);
 
 
 int main() {
     SPACER;
-    printf("\n        Balanced Parenthesis Checker\n\n");
+    printf("\n        Balanced Brackets Checker\n\n");
     SPACER;
     
     char exp[MAX], response='y', trash[MAX];
@@ -20,9 +20,9 @@ int main() {
         scanf("%s", &exp);
         printf("\n");
 
-        isParenthesisBalanced(exp) ?
-            printf("        Parenthesis Are Balanced\n") :
-            printf("        Parenthesis Are Not Balanced\n");
+        isBracketsBalanced(exp) ?
+            printf("        Brackets Are Balanced\n") :
+            printf("        Brackets Are Not Balanced\n");
         SPACER;
         
         printf("\nCheck another expression?\n");
@@ -38,45 +38,40 @@ int main() {
 }
 
 
-// Check if the two input parameters are matching parenthesis
-int checkPair(char item1,char item2){
-    return (( item1=='(' && item2==')' )||( item1=='[' && item2==']' )||( item1=='{' && item2=='}' ));
-}
-
-
-// Check if every opening parenthesis "(-{-[" has the corresponding closing one ")-}-]"
+// Check if every opening bracket "(-{-[" has the corresponding closing one ")-}-]"
 // It takes a string that contains the expression to be checked
 // Characters other than "(-{-[-]-}-)" are normally ignored
-int isParenthesisBalanced(char exp[]){
+// Return "1" if all the brackets are perfectly matched, returns "0" otherwise 
+int isBracketsBalanced(char exp[]){
 
     struct stack myStack;
     myStack.top = -1;
       
     for (int i = 0; exp[i]!='\0'; i++){
 
-        // If it is an opening parenthesis "(, [, {", push it to the stack 
+        // If it is an opening bracket "(, [, {", push it to the stack 
         if(exp[i] == '(' || exp[i] == '[' || exp[i] == '{'){
             push(&myStack, exp[i]); 
         }
 
-        // If it is a closing parenthesis
+        // If it is a closing bracket
         else if(exp[i] == ')' || exp[i] == ']' || exp[i] == '}'){
 
             // Check if the stack is empty, and there is no corresponding opening one
             if (myStack.top == -1){
-                printf("The closing parenthesis \'%c\' at index %d has no correspoding opening\n",
+                printf("The closing bracket \'%c\' at index %d has no correspoding opening\n",
                 exp[i], i);
                 //SPACER;
                 return 0;
             }
-            // Check if it is NOT matching with the last opened parenthesis (top of stack)
+            // Check if it is NOT matching with the last opened bracket (top of stack)
             else if( !checkPair(myStack.stck[myStack.top], exp[i]) ){
-                printf("The closing parenthesis \'%c\' at index %d is not matching with the last opened parenthesis \'%c\'\n",
-                exp[i], i, myStack.top);
+                printf("The closing bracket \'%c\' at index %d is not matching with the last opened bracket \'%c\'\n",
+                exp[i], i, myStack.stck[myStack.top]);
                 //SPACER;
                 return 0;
             }
-            // Ensure that it is matching with the last opened parenthesis (top of stack)
+            // Ensure that it is matching with the last opened bracket (top of stack)
             else if(checkPair(myStack.stck[myStack.top], exp[i])){
                 pop(&myStack);
             }
@@ -84,12 +79,29 @@ int isParenthesisBalanced(char exp[]){
     }
     // Check if the stack is still NOT empty after parsing the whole expression
     if(myStack.top>-1){
-        printf("There are %d opened parenthesis at the end of expression which are not closed\n", myStack.top+1);
+        printf("There are %d opened bracket which are not closed\n", myStack.top+1);
         //SPACER;
         return 0;
     }
-    // Check that the stack is empty (all opened parenthesis are properly closed)
+    // Check that the stack is empty (all opened bracket are properly closed)
     else if(myStack.top == -1){
         return 1;
     }
 }
+
+
+
+// Check if the two input parameters are matching bracket
+// Return "1" if they are matching, returns "0" otherwise 
+int checkPair(char item1,char item2){
+    return (( item1=='(' && item2==')' )||( item1=='[' && item2==']' )||( item1=='{' && item2=='}' ));
+}
+
+
+/*
+Test Cases:
+(3+5)*(4-9)+{[8/9]}+[(5*98)]
+)(4+5)*(6-9)
+{(4+9))}[{hello}+(5/7)]
+[1]*[3]{(}
+*/
